6a5c06999b7e3e26fe740027186f18869c4cfb11
dajacobs <dajacobs.tab@gmail.com>
2015-10-21 08:51:03 -0500

    sort test initialize

---
+++ jSrc/src/javaShell/SortTest.java
@@ -0,0 +1,9 @@
+package javaShell;
+
+public class SortTest {
+    public static void main(String[] args) {
+        Integer[] list1 = {7, 12, 19, 3, 18, 4, 2, 6, 15, 11};
+        Integer[] list2 = {16, 1, 5, 9, 13, 20, 17, 8, 10, 14};
+        Object finalList[] = new Object[list1.length + list2.length];
+    }
+}

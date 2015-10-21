19489d8b03bbd27e77dec960a92dc3fb6ffef866
dajacobs <dajacobs.tab@gmail.com>
2015-10-21 08:41:27 -0500

    run func

--- jSrc/src/javaShell/MergeThread.java
+++ jSrc/src/javaShell/MergeThread.java
@@ -12,7 +12,26 @@ public class MergeThread implements Runnable {
         this.mList = mList;
     }
     // Runnable method
+    @Override
     public void run() {
-        
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        while((x < list1.length) && y < list2.length) {
+            if(((Comparable)list1[x]).compareTo(list2[y]) < 0) {
+                mList[z++] = list1[x++];
+            } else {
+                mList[z++] = list2[y++];
+            }
+        }
+        if(x < list1.length) {
+            for(int i = x; i < list1.length; i++) {
+                mList[z++] = list1[i];
+            }
+        } else {
+            for(int i = y; i < list2.length; i++) {
+                mList[z++] = list2[i];
+            }
+        }
     }
 }

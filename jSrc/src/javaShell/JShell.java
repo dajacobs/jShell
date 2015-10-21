37f7ad325e46e36dacd2668e68f9439993a3251e
dajacobs <dajacobs.tab@gmail.com>
2015-10-21 11:15:55 -0500

    history func

--- jSrc/src/javaShell/JShell.java
+++ jSrc/src/javaShell/JShell.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class changeDir {
+public class JShell {
     public static String slash = "/";
     public static void main(String[] args) throws IOException {
         String commandLine;
@@ -25,31 +25,38 @@ public class changeDir {
             }
             // Save commands in array list
             StringTokenizer st = new StringTokenizer(commandLine);
-            ArrayList<String> commands = new ArrayList<>();
-            
+            ArrayList<String> history = new ArrayList<>();
+       
             while(st.hasMoreTokens()) {
-                commands.add(st.nextToken());
+                history.add(st.nextToken());
             }
             // Change directory command
-            if(commands.contains("cd")) {
-                if(commands.get(commands.size() - 1).equals("cd")) {
+            if(history.contains("cd")) {
+                if(history.get(history.size() - 1).equals("cd")) {
                     File home = new File(System.getProperty("user.home"));
                     System.out.println("Home directory: " +home);
                     pb.directory(home);
                     continue;
                 } else {
-                    String arg = commands.get(commands.size() - 1);
+                    String arg = history.get(history.size() - 1);
                     System.out.println("Arguement passed: " +arg);
                     String cPath = pb.directory() +slash +arg;
                     System.out.println("Path created: " +cPath);
                     File nPath = new File(cPath);
                     pb.directory(nPath);
                     continue;
+                }    
+            } else if(history.contains("history")) {
+                if(history.get(history.size() - 1).equals("history")) {
+                    for(int i = 0; i > history.size() - 1; i++) {
+                        System.out.println("["+i+"]" +history.get(i));
+                    }
+                    continue;
                 }
             }
             // Pass the commands to ProcessBuilder command
-            pb.command(commands);
-            Process proc  = pb.start();
+            pb.command(history);
+            Process proc = pb.start();
             
             // Output stream to be returned from the proc
             InputStream is = proc.getInputStream();

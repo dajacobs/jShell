5dd69428c1aac0482af3a92552f05ba6735b23c0
dajacobs <dajacobs.tab@gmail.com>
2015-10-21 09:38:18 -0500

    cd conditional

--- jSrc/src/javaShell/changeDir.java
+++ jSrc/src/javaShell/changeDir.java
@@ -1,6 +1,7 @@
 package javaShell;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -23,6 +24,26 @@ public class changeDir {
             // Save commands in array list
             StringTokenizer st = new StringTokenizer(commandLine);
             ArrayList<String> commands = new ArrayList<>();
+            
+            while(st.hasMoreTokens()) {
+                commands.add(st.nextToken());
+            }
+            // Change directory command
+            if(commands.contains("cd")) {
+                if(commands.get(commands.size() - 1).equals("cd")) {
+                    File home = new File(System.getProperty("user.home"));
+                    System.out.println("Home directory: " +home);
+                    pb.directory(home);
+                    continue;
+                } else {
+                    String arg = commands.get(commands.size() - 1);
+                    System.out.println("Arguement passed: " +arg);
+                    String cPath = pb.directory() +backSlash +arg;
+                    System.out.println("Path created: " +cPath);
+                    File nPath = new File(cPath);
+                    pb.directory(nPath);
+                    continue;
+                }
             }
         }
     }
